119f48414455917011c38804a2a08a5afbd59a99
SebastienRico <rico.2.sebastien@gmail.com>
2018-12-04 21:47:22 +0100

    Début branch and bound

--- TSP/src/tsp/controllers/BranchAndBoundController.java
+++ TSP/src/tsp/controllers/BranchAndBoundController.java
@@ -1,104 +1,158 @@
 package tsp.controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import tsp.models.Graph;
-import tsp.models.Node;
+import tsp.models.Vertex;
 
 public class BranchAndBoundController implements SolutionControllerInterface{
     
-    private static Boolean begining = true;
-
+    private List<Vertex> pathToFollow = new ArrayList<>();
+    
     @Override
     public void resolveGraph(Graph basicGraph) {}
-    
-    // créer un noeud
-    // calculer le poids du noeud (méthode bound)
-    // chercher les possibilités suivantes (méthode branch)
-    // pour chaque possibilité
-        // ajouter la liste des chemins
-        // appeler la méthode précédente
 
     @Override
-    public void resolveTable(Double[][] table) {
-        /*if(begining){
-            // on dit que le maximum optimal est infinit
-            Double minForAlgo = -1d;
-            int sizeOfTable = table.length;
-            Double[][] reducedTable = table;
-            List<Node> path = new ArrayList<>();
-            Node currentNode;
-
-            // on part du principe qu'on commence par la ville 0
-            // on crée le premier node
-            Node initialNode = new Node();
-            initialNode.node = 0;
-            initialNode.originTable = table;
-            initialNode.sizeOfTable = sizeOfTable;
-            initialNode.path.add(0);
-            
-            begining = false;
+    public void resolveTable(Double[][] matrice) {
+        
+        int size = matrice.length;
+        
+        Map<Vertex, Double[][]> reducedMatrixForEachNode = new HashMap();
+        List<Vertex> nodesToVisit = getAllNodesToVisit(matrice);
+        
+        
+        // si aucune ville n'a encore été visitée ou si toutes les villes ont été visitées
+        if(nodesToVisit.isEmpty()){
+            System.out.println("Toutes les villes sont visitées");
+            return;
+        } else {
+            pathToFollow.add(nodesToVisit.get(0));
+            nodesToVisit.remove(nodesToVisit.get(0));
         }
         
-        // on calcule le poids du noeud
-        bound(currentNode, minForAlgo);
         
-        path.add(initialNode);
-        currentNode = initialNode;
-
-        while(path.size()<sizeOfTable){
-            branch(currentNode);
-        }*/
+        // pour les prochaines villes
+        nodesToVisit.forEach(prochaineVille -> {
+            Double[][] reducedMatrix = new Double[size][size];//matrice;
+            
+            for(int i =0; i<size; i++){
+                for(int j =0; j<size; j++){
+                    reducedMatrix[i][j] = matrice[i][j];
+                }
+            }
+            
+            // on cherche le minimum de la ligne de la matrice et on le soustrait à la ligne
+            for(int i = 0; i < size; i++){
+                Double minOfLine = Double.POSITIVE_INFINITY;
+                for(int j = 0; j < size; j++){
+                    if(reducedMatrix[i][j] < minOfLine && reducedMatrix[i][j] != Double.POSITIVE_INFINITY){
+                        minOfLine = reducedMatrix[i][j];
+                    }
+                }
+                for(int j = 0; j < size; j++){
+                    if(reducedMatrix[i][j] != Double.POSITIVE_INFINITY){
+                        reducedMatrix[i][j] = reducedMatrix[i][j] - minOfLine;
+                    }
+                }
+                prochaineVille.setWeight(prochaineVille.getWeight() + minOfLine);
+            }
+            
+            // on cherche le minimum de la colonne de la matrice et on le soustrait à la colonne
+            for(int i = 0; i < size; i++){
+                Double minOfColumn = Double.POSITIVE_INFINITY;
+                for(int j = 0; j < size; j++){
+                    if(reducedMatrix[j][i] < minOfColumn && reducedMatrix[j][i] != Double.POSITIVE_INFINITY){
+                        minOfColumn = reducedMatrix[j][i];
+                    }
+                }
+                for(int j = 0; j < size; j++){
+                    if(reducedMatrix[j][i] != Double.POSITIVE_INFINITY){
+                        reducedMatrix[j][i] = reducedMatrix[j][i] - minOfColumn;
+                    }
+                }
+                prochaineVille.setWeight(prochaineVille.getWeight() + minOfColumn);
+            }
+            
+            // on nettoie le reste de la matrice
+            for(int i = 0; i < size; i++){
+                for(int j = 0; j < size; j++){
+                    if(iAndJInFinalPath(i,j)){
+                        reducedMatrix[i][j] = Double.POSITIVE_INFINITY;
+                    }
+                    if(j == pathToFollow.get(0).getIdVertex() - 1){
+                        reducedMatrix[i][j] = Double.POSITIVE_INFINITY;
+                    }
+                    if(j == prochaineVille.getIdVertex() -1){
+                        reducedMatrix[j][i] = Double.POSITIVE_INFINITY;
+                    }
+                }
+            }
+            
+            // on ajoute à la map de villes associées à leur matrice
+            reducedMatrixForEachNode.put(prochaineVille, reducedMatrix);
+        });
+        
+        // on cherche le ou les poids le ou les plus petit dans la map
+        Map<Vertex, Double[][]> smallerVerteces = getNextNodes(reducedMatrixForEachNode);
+        
+        // ici boucler mais si jamais le poids devient trop important il laisser tomber et revenir sur un moins gros
+        smallerVerteces.forEach((k,v) -> {
+            //getFirst ?
+            System.out.println("On boucle : " + k.getIdVertex());
+        });
+        
     }
 
-    /*private void branch(NodeForBranchAndBound currentNode) {
-        // lister les branches possibles
-        List<Integer> possibleNextNodes = new ArrayList<>();
-        for(int i = 0; i < currentNode.originTable.length; i++){
-            NodeForBranchAndBound node = new NodeForBranchAndBound();
-            node.node = i;
-            node.originTable = currentNode.reducedTable;
-            node.path.add(node.node);
-            node.cost = currentNode.originTable[currentNode.node][i] + currentNode.cost;
+    private boolean iAndJInFinalPath(int i, int j) {
+        List<Integer> villesVisitees = new ArrayList<>();
+        pathToFollow.forEach(v -> {
+            villesVisitees.add(v.getIdVertex());
+        });
+        if(villesVisitees.contains(i) && villesVisitees.contains(j)){
+            return true;
         }
-        // pour chaque branche créer un neoud
-            // calculer le poids
-            // réduire la matrice
-            // vérifier quel poids
-        // retourner le noeud choisi
+        return false;
     }
     
-    private void bound(Node currentNode, Double minForAlgo){
-        Double weightOfTable = 0d;
-        // on cherche le minimunm de la ligne qu'on retranche à la ligne et qu'on ajoute au poids total du tableau
-        for(int i = 0; i < currentNode.sizeOfTable; i++){
-            Double minOfLine = currentNode.originTable[i][0];
-            for(int j = 0; j < currentNode.sizeOfTable; j++){
-                if(currentNode.originTable[i][j] < minOfLine){
-                    minOfLine = currentNode.originTable[i][j];
+    private List<Vertex> getAllNodesToVisit(Double[][] matrice){
+        int size = matrice.length;
+        List<Vertex> nodesToVisit = new ArrayList<>();
+        
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(matrice[i][j] != Double.POSITIVE_INFINITY){
+                    Vertex v = new Vertex(i + 1);
+                    nodesToVisit.add(v);
+                    if(i + 1 < size){
+                        i++;
+                    } else {
+                        break;
+                    }
+                    j=-1;
                 }
             }
-            for(int j = 0; j < currentNode.sizeOfTable; j++){
-                currentNode.reducedTable[i][j] = currentNode.originTable[i][j] - minOfLine;
-            }
-            weightOfTable = weightOfTable + minOfLine;
         }
-        // on cherche le minimum de la colonne qu'on retranche à la colonne et qu'on ajoute au poids total du tableau
-        for(int i = 0; i < currentNode.sizeOfTable; i++){
-            Double minOfColumn = currentNode.originTable[0][i];
-            for(int j = 0; j < currentNode.sizeOfTable; j++){
-                if(currentNode.originTable[j][i] < minOfColumn){
-                    minOfColumn = currentNode.originTable[j][i];
-                }
+        return nodesToVisit;
+    }
+    
+    private Map<Vertex, Double[][]> getNextNodes(Map<Vertex, Double[][]> reducedMatrixForEachNode){
+        // on cherche le ou les poids le ou les plus petit dans la map
+        Double minForChild = Double.POSITIVE_INFINITY;
+        for(Map.Entry<Vertex, Double[][]> entry : reducedMatrixForEachNode.entrySet()){
+            if(entry.getKey().getWeight() <= minForChild){
+                minForChild = entry.getKey().getWeight();
             }
-            for(int j = 0; j < currentNode.sizeOfTable; j++){
-                currentNode.reducedTable[j][i] = currentNode.originTable[j][i] - minOfColumn;
+        }
+        Map<Vertex, Double[][]> smallerVerteces = new HashMap<>();
+        for(Map.Entry<Vertex, Double[][]> entry : reducedMatrixForEachNode.entrySet()){
+            Boolean t = entry.getKey().getWeight() == minForChild;
+            if(entry.getKey().getWeight().equals(minForChild)){
+                smallerVerteces.put(entry.getKey(), entry.getValue());
             }
-            weightOfTable = weightOfTable + minOfColumn;
         }
-        // si on a parcouru un premier chemin
-        if(minForAlgo == -1 && currentNode.path.size() == 1){
-            minForAlgo = weightOfTable;
-        }
-    }*/
+        return smallerVerteces;
+    }
+
 }
--- TSP/src/tsp/models/Vertex.java
+++ TSP/src/tsp/models/Vertex.java
@@ -1,16 +1,18 @@
 package tsp.models;
 
-import java.util.List;
-
 public class Vertex {
     
-    private Integer idVertex;
-    private Integer idNode;
-    private List<Vertex> previousVertex;
-    private List<Vertex> nextVertex;
+    private Integer idVertex; // corespond to the idNode of node
     private Double weight;
     
-    public Vertex(){}
+    public Vertex(){
+        this.weight = 0d;
+    }
+    
+    public Vertex(Integer idVertex){
+        this.idVertex = idVertex;
+        this.weight = 0d;
+    }
     
     public Integer getIdVertex(){
         return idVertex;
@@ -18,30 +20,6 @@ public class Vertex {
     
     public void setIdVertex(Integer idVertex){
         this.idVertex = idVertex;
-    }
-    
-    public Integer getIdNode(){
-        return idNode;
-    }
-    
-    public void setIdNode(Integer idNode){
-        this.idNode = idNode;
-    }
-    
-    public List<Vertex> getPreviousVertex(){
-        return previousVertex;
-    }
-    
-    public void setPreviousVertex(List<Vertex> previousVertex){
-        this.previousVertex = previousVertex;
-    }
-    
-    public List<Vertex> getNextVertex(){
-        return nextVertex;
-    }
-    
-    public void setNextVertex(List<Vertex> nextVertex){
-        this.nextVertex = nextVertex;
     }
     
     public Double getWeight(){
